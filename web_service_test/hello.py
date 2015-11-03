f911793cf1df46d38907bbabb7a8e78da84e10f5
laiy <ly.franky@gmail.com>
2015-11-03 09:58:32 +0800

    fucking tornado version

---
+++ web_service_test/hello.py
@@ -0,0 +1,13 @@
+import tornado.web
+import tornado.wsgi
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+wsgi_app = tornado.wsgi.WSGIAdapter(application)
